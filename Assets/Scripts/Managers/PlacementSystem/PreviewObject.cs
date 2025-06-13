94233a6b9b2cbab12053bc41976c85abd637e757
Justwouter <wouter2312@gmail.com>
2025-06-13 11:01:48 +0200

    Fix collider errors

--- Assets/Scripts/Managers/PlacementSystem/PreviewObject.cs
+++ Assets/Scripts/Managers/PlacementSystem/PreviewObject.cs
@@ -1,58 +1,59 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PreviewObject : MonoBehaviour {
 
-    private void Start() {
-        int layer = LayerMask.NameToLayer("Ignore Raycast");
-        gameObject.layer = layer;
+	public List<Collider> Colliders = new();
 
-        if (TryGetComponent(out MeshRenderer renderer)) {
-            if (!TryGetComponent(out Collider collider)) {
-                MeshCollider meshCollider = gameObject.AddComponent<MeshCollider>();
-                meshCollider.convex = true;
-                meshCollider.isTrigger = true;
-                if (!TryGetComponent(out Rigidbody rigidbody)) {
-                    rigidbody = gameObject.AddComponent<Rigidbody>();
-                    rigidbody.isKinematic = true;
-                    rigidbody.useGravity = false;
-                }
-            }
-        }
+	private void Start() {
+		int layer = LayerMask.NameToLayer("Ignore Raycast");
+		gameObject.layer = layer;
 
-        MeshRenderer[] renderers = GetComponentsInChildren<MeshRenderer>();
-        foreach (MeshRenderer childRenderer in renderers) {
-            childRenderer.gameObject.layer = layer;
-            if (childRenderer.gameObject.TryGetComponent<Collider>(out Collider collider)) {
-                continue;
-            }
+		if (TryGetComponent(out MeshRenderer renderer)) {
+			if (!TryGetComponent(out Collider collider)) {
+				MeshCollider meshCollider = gameObject.AddComponent<MeshCollider>();
+				meshCollider.convex = true;
+				meshCollider.isTrigger = true;
+				if (!TryGetComponent(out Rigidbody rigidbody)) {
+					rigidbody = gameObject.AddComponent<Rigidbody>();
+					rigidbody.isKinematic = true;
+					rigidbody.useGravity = false;
+				}
+			}
+		}
 
-            MeshCollider meshCollider = childRenderer.gameObject.AddComponent<MeshCollider>();
-            meshCollider.convex = true;
-            meshCollider.isTrigger = true;
-            print("Mesh done");
-            
-        }
-        
-    }
-    public List<Collider> Colliders = new List<Collider>();
-    private void OnTriggerEnter(Collider collider) {
-        if (collider.gameObject.layer == LayerMask.NameToLayer("Ground") || collider.gameObject.layer == LayerMask.NameToLayer("Buildable")) {
-        }
+		MeshRenderer[] renderers = GetComponentsInChildren<MeshRenderer>();
+		foreach (MeshRenderer childRenderer in renderers) {
+			childRenderer.gameObject.layer = layer;
+			if (childRenderer.gameObject.TryGetComponent<Collider>(out Collider collider)) {
+				continue;
+			}
 
-        else {
-            Colliders.Add(collider);
-        }
+			MeshCollider meshCollider = childRenderer.gameObject.AddComponent<MeshCollider>();
+			meshCollider.convex = true;
+			meshCollider.isTrigger = true;
+			print("Mesh done");
 
-    }
+		}
 
-    private void OnTriggerExit(Collider collider) {
-        if (collider.gameObject.layer == LayerMask.NameToLayer("Ground") || collider.gameObject.layer == LayerMask.NameToLayer("Buildable")) {
-        }
+	}
 
-        else {
-            Colliders.Remove(collider);
-        }
+	void Update() {
+		Colliders = Colliders.Where(c => c != null).ToList();
+	}
 
-    }
+
+	private void OnTriggerEnter(Collider collider) {
+		if (!(collider.gameObject.layer == LayerMask.NameToLayer("Ground") || collider.gameObject.layer == LayerMask.NameToLayer("Buildable"))) {
+			Colliders.Add(collider);
+		}
+	}
+
+	private void OnTriggerExit(Collider collider) {
+		if (!(collider.gameObject.layer == LayerMask.NameToLayer("Ground") || collider.gameObject.layer == LayerMask.NameToLayer("Buildable"))) {
+			Colliders.Remove(collider);
+		}
+
+	}
 }
